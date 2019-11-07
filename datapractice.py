6a6463d91ff8fceb974983c18b94e1062ae82ab6
KyeLee143 <54326810+KyeLee143@users.noreply.github.com>
2019-11-07 09:44:08 -0800

    Add files via upload

---
+++ datapractice.py
@@ -0,0 +1,12 @@
+month = {"oct":"10","nov":"11","dec":"12"}
+month["may"]="5"
+
+
+
+student = {"name":"Juvy"}
+number = {"age":'16'}
+schedule = {"P1":"math","P2":"English","P3":"apush","P4":"physics"}
+
+info = {"name":{"Juvy"}, "age":{'16'}}
+
+print(number["age"])
